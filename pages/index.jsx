571d3649012a7ff943d933bb62a97ee28e0e3f0b
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-01-07 18:44:12 -0300

    Criado componente input

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import BackgroundImageWithChildren from "../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren";
 import Logo from "../src/components/logo/Logo";
+import Input from "../src/components/form/input/Input";
 
 const StyledDiv = styled.div`
   background-color: ${(props) => props.theme.colors.white};
@@ -10,7 +11,6 @@ const StyledDiv = styled.div`
   flex-direction: column;
   justify-content: center;
   padding: 0 40px;
-
 
   @media (max-width: 500px) {
     min-width: 100%;
@@ -21,8 +21,8 @@ export default function HomePage() {
   return (
     <BackgroundImageWithChildren>
       <StyledDiv>
-        <Logo showImage type={'vertical'}/>
-        <input />
+        <Logo showImage type={"vertical"} />
+        <Input placeholder="Digite o nome da cidade"/>
       </StyledDiv>
     </BackgroundImageWithChildren>
   );
