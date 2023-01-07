0771830c6a41f5cf97195e0a06c58e6991d69f56
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-01-07 18:14:48 -0300

    Criado componente logo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,29 @@
 import styled from "styled-components";
 
 import BackgroundImageWithChildren from "../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren";
+import Logo from "../src/components/logo/Logo";
 
 const StyledDiv = styled.div`
-  background-color: ${props => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.white};
   min-width: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 40px;
+
 
   @media (max-width: 500px) {
     min-width: 100%;
   }
-
 `;
 
 export default function HomePage() {
   return (
     <BackgroundImageWithChildren>
-      <StyledDiv>Página Inicial</StyledDiv>
+      <StyledDiv>
+        <Logo showImage type={'vertical'}/>
+        <input />
+      </StyledDiv>
     </BackgroundImageWithChildren>
   );
 }
