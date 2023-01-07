091132e7f23d9e2465ed2e94339af8042fa2968e
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-01-07 15:43:34 -0300

    Melhorado index

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,13 @@ import styled from "styled-components";
 import BackgroundImageWithChildren from "../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren";
 
 const StyledDiv = styled.div`
-  background-color: ${(props) => props.theme.colors.black};
+  background-color: ${props => props.theme.colors.white};
+  min-width: 500px;
+
+  @media (max-width: 500px) {
+    min-width: 100%;
+  }
+
 `;
 
 export default function HomePage() {
