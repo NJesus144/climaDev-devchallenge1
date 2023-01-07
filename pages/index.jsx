1ec5b4c38c940cc4d3847b90644d2d4431639d38
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-01-07 15:40:47 -0300

    Criado background image

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
+import BackgroundImageWithChildren from "../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren";
+
 const StyledDiv = styled.div`
-  background-color: ${props => props.theme.colors.black}
+  background-color: ${(props) => props.theme.colors.black};
 `;
 
 export default function HomePage() {
-  return(
-     <StyledDiv>Páginas inicial 123</StyledDiv>
-     
-     );
+  return (
+    <BackgroundImageWithChildren>
+      <StyledDiv>Página Inicial</StyledDiv>
+    </BackgroundImageWithChildren>
+  );
 }
