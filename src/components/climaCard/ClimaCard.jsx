3b97c4ca59471795b70a7233fe418e23a0d7c1af
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-01-08 20:35:40 -0300

    Criado integração com API

--- src/components/climaCard/ClimaCard.jsx
+++ src/components/climaCard/ClimaCard.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 const StyledClimaCard = styled.div`
-  background-color: ${(props) => props.theme.colors.white};
+  background-color: ${props => props.theme.colors.white};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
   width: 350px;
   padding: 15px;
   box-sizing: border-box;
-`;
+`
 
 const Title = styled.h6`
   font-weight: 700;
@@ -15,57 +15,60 @@ const Title = styled.h6`
   text-align: center;
   margin: 0;
   padding: 0;
-`;
+`
 
 const BasicInfo = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`;
+`
 
 const BasicInfoImg = styled.img`
   width: 100px;
   height: 100px;
-`;
+`
 const BasicInfoText = styled.div`
   text-align: center;
   flex: 1;
-`;
+`
 
 const InfoLine = styled.p`
   display: flex;
   justify-content: space-between;
   font-size: 20px;
   padding: 0 15px;
-`;
+`
 
 const InfoTemperature = styled.span`
   font-weight: 700;
 
-  ${(props) => props.blue && "color: #00C9F5;"}
-  ${(props) => props.red && "color: #FF0000;"}
-`;
+  ${props => props.blue && 'color: #00C9F5;'}
+  ${props => props.red && 'color: #FF0000;'}
 
-export default function ClimaCard() {
+`
+
+export default function ClimaCard({
+  title, icon, description, temp, feels, min, max
+}){
   return (
     <StyledClimaCard>
-      <Title>Titulo</Title>
+      <Title>{title}</Title>
       <BasicInfo>
-        <BasicInfoImg src="http://openweathermap.org/img/wn/02d@2x.png" />
-        <BasicInfoText>Algumas nuvens</BasicInfoText>
+        <BasicInfoImg src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+        <BasicInfoText>{description}</BasicInfoText>
       </BasicInfo>
       <InfoLine>
-        Temperatura<InfoTemperature>29°C</InfoTemperature>
+        Temperatura<InfoTemperature>{temp}</InfoTemperature>
       </InfoLine>
       <InfoLine>
-        Sensação<InfoTemperature>29°C</InfoTemperature>
+        Sensação<InfoTemperature>{feels}</InfoTemperature>
       </InfoLine>
       <InfoLine>
-        Mínima<InfoTemperature blue>29°C</InfoTemperature>
+        Mínima<InfoTemperature blue >{min}</InfoTemperature>
       </InfoLine>
       <InfoLine>
-        Máxima<InfoTemperature red>29°C</InfoTemperature>
+        Máxima<InfoTemperature red >{max}</InfoTemperature>
       </InfoLine>
     </StyledClimaCard>
-  );
+  )
 }
